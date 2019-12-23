6712df67c8b845d9a4959c609443edffe3a5e7b6
Will Carter <will.carter@fergusllc.com>
2019-12-23 20:44:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,12 +10,22 @@ def summon_captain_planet(calls)
     "#{call.capitalize}!"
   end
 end
-planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-puts summon_captain_planet(planeteer_calls).inspect
+#planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+#puts summon_captain_planet(planeteer_calls).inspect
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  returnVal = false
+  calls.each { |call|
+    if call.length > 4
+      returnVal = true
+    end
+  }
+  returnVal
 end
+
+short_words = ["puff", "go", "two"]
+puts long_planeteer_calls(short_words)
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
