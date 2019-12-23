42aab2903a67660d9dedfcf5957e84e96a18c89b
Will Carter <will.carter@fergusllc.com>
2019-12-23 21:00:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,21 +1,21 @@
-def roll_call_dwarves(dwarves)
-  dwarves.each_with_index { |item, index|
-    puts "#{index + 1} #{item}"
-  }
-end
+# def roll_call_dwarves(dwarves)
+#   dwarves.each_with_index { |item, index|
+#     puts "#{index + 1} #{item}"
+#   }
+# end
 #roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
 
-def summon_captain_planet(calls)
-  return calls.map do |call|
-    "#{call.capitalize}!"
-  end
-end
+# def summon_captain_planet(calls)
+#   return calls.map do |call|
+#     "#{call.capitalize}!"
+#   end
+# end
 #planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 #puts summon_captain_planet(planeteer_calls).inspect
 
-def long_planeteer_calls(calls)
-  return calls.any? { |call| call.length > 4 }
-end
+# def long_planeteer_calls(calls)
+#   return calls.any? { |call| call.length > 4 }
+# end
 
 # short_words = ["puff", "go", "two"]
 # puts long_planeteer_calls(short_words)
